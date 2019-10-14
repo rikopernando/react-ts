8821312c645bcf26c6a43bf5602cb353a5e008fe
rikopernando <riko.pernando@bareksa.com>
2019-10-14 17:30:21 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Task } from './models/task';
+import { NewTaskForm } from './components/NewTaskForm'
+import { TaskList } from './components/TaskList'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  newTask: Task
+  tasks: Task[]
 }
 
-export default App;
+class App extends Component<{}, State> {
+
+  state = {
+    newTask: {
+      id: 1,
+      name: ''
+    },
+    tasks: []
+  }
+
+  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    this.setState(previousState => ({
+      newTask: {
+        id: previousState.newTask.id + 1,
+        name: ''
+      },
+      tasks: [...previousState.tasks, previousState.newTask]
+    }))
+  }
+
+  private deleteTask = (taskToDelete: Task) => {
+    this.setState(previouseState => ({
+      tasks: [
+        ...previouseState.tasks.filter(task => task.id !== taskToDelete.id)
+      ]
+    }))
+  }
+
+  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTask: {
+        ...this.state.newTask,
+        name: event.target.value
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello React TS!</h2>
+        <NewTaskForm
+          task={this.state.newTask}
+          onAdd={this.addTask}
+          onChange={this.handleTaskChange}
+        />
+        <TaskList
+          tasks={this.state.tasks}
+          onDelete={this.deleteTask}
+        />
+      </div>
+    );
+  }
+}
+
+
+export default App
---
+++ src/components/TaskListItem.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react'
+
+import { Task } from '../models/task'
+
+interface Props {
+    task: Task
+    onDelete: (task: Task) => void
+}
+
+export const TaskListItem: FunctionComponent<Props> = ({task, onDelete }) => {
+    const onClick = () => {
+        onDelete(task)
+    }
+
+    return (
+        <li>
+            {task.name} <button onClick={onClick}>x</button>
+        </li>
+    )
+}
